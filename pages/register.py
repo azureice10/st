e18bcef38c6d94ea9069434a236a75048bbefbc8
Budiarto <52266413+azureice10@users.noreply.github.com>
2024-01-15 10:18:09 +0700

    Update register.py

--- pages/register.py
+++ pages/register.py
@@ -5,7 +5,7 @@ from yaml.loader import SafeLoader
 
 st.title("Register New User")
 
-with open('../config.yml') as file:
+with open('config.yml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 authenticator = stauth.Authenticate(
@@ -17,7 +17,7 @@ authenticator = stauth.Authenticate(
 
 try:
     if authenticator.register_user('Register user', preauthorization=False):
-        with open('../config.yml', 'w') as file:
+        with open('config.yml', 'w') as file:
             yaml.dump(config, file, default_flow_style=False)
             st.success('User registered successfully')
 except Exception as e:
