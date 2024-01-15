7faf8d94c7683f65323f0735d7f17ee4ad19c43a
Budiarto <52266413+azureice10@users.noreply.github.com>
2024-01-15 09:48:28 +0700

    Update register.py

--- pages/register.py
+++ pages/register.py
@@ -5,20 +5,3 @@ from yaml.loader import SafeLoader
 
 st.title("Register New User")
 
-with open('C:/Users/raden/OneDrive/Documents/st/config.yml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days']
-)
-
-try:
-    if authenticator.register_user('Register user', preauthorization=False):
-        with open('C:/Users/raden/OneDrive/Documents/st/config.yml', 'w') as file:
-            yaml.dump(config, file, default_flow_style=False)
-            st.success('User registered successfully')
-except Exception as e:
-    st.error(e)
